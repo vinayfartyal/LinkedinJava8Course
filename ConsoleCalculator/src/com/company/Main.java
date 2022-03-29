86bc63cdb8e99d32604b607b7a31567fac80ea7e
vinfarty <vinay.fartyal@publicissapient.com>
2022-03-29 14:40:05 +0530

    Initial Commit

---
+++ ConsoleCalculator/src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Select the Operation:");
+        System.out.println("1. Addition");
+        System.out.println("2. Subtraction");
+        System.out.println("3. Multiplication");
+        System.out.println("4. Division");
+        System.out.println("5. ");
+        do {
+
+        }
+    }
+}
